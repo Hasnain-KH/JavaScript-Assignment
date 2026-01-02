7155764eceb5e8c2d82fc0c001fc9c7b260c4e58
Muhammadhasnain <webdevhasnain@gmail.com>
2026-01-02 23:41:05 +0500

    Update

---
+++ Asssignment No  PDF 26 - 30/app.js
@@ -0,0 +1,66 @@
+// q 1
+
+// var user = prompt("\"Enter positive integers .\"");
+// console.log("Number" + " " + user);
+// console.log("ound off value" + " " +  Math.round(user));
+// console.log("flour value" + " " + Math.floor(user));
+// console.log("ceil value" + " " +  Math.ceil(user));
+
+// // q 2
+
+// var  user = prompt("\"Enter Negative integers . \"");
+// console.log("Number" + " " + user);
+// console.log("round off value" + " " + Math.round(user));
+// console.log("floor value" + " " + Math.floor(user));
+// console.log("ceil" + " " + Math.ceil(user));
+
+// q 3
+
+// var dice = Math.floor(Math.random() * 4 + 1);
+// console.log(dice);
+// var dice = Math.floor(Math.random() * 6 + 1);
+// console.log(dice);
+
+// q 4
+
+// var num = -4;
+// console.log(num);
+// var result = Math.abs(num);
+// console.log(result);
+
+// q 5
+
+var toes = Math.floor(Math.random() * 2);
+console.log(toes);
+if(toes === 0){
+    document.write("Heads")
+}else{
+    document.write("Tail")
+}
+
+
+
+
+ 
+// q 6
+
+// console.log("random number between 1 and 100" + ":" + 
+// Math.floor(Math.random() * 100 + 1));
+
+// q 7;
+
+// var weight = prompt("Enter your weight here");
+// var parsweight = parseFloat(weight);
+// document.write(parsweight + "kg");
+
+// q 8
+
+
+// var secretNumber = Math.floor(Math.random() * 10 + 1);
+// var user = prompt("Enter her number between 1 - 10");
+// var user = Number(user);
+// if(user === secretNumber){
+//     alert("\"Welcome congratulate\"");
+// }else{
+// alert("Sorry! The secrete number was " + secretNumber);
+// }
