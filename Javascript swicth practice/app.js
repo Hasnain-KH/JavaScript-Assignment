a759458bc86d227e82b39a7810caedb3e15fc8fe
Muhammadhasnain <webdevhasnain@gmail.com>
2026-01-04 22:34:00 +0500

    update

---
+++ Javascript swicth practice/app.js
@@ -0,0 +1,67 @@
+var day = prompt("\"Enter here a days name .\"");
+switch (day){
+    case "sunday":
+    alert("Today is holiday");
+    break;
+    case "monday":
+    alert("Today is a work day");
+    break;
+    case "tuesday":
+    alert("today is a also work day");
+    break;
+    case "wednesday":
+    alert("Today will be the sport day in school");
+    break;
+    case "thursday":
+    alert("Today is a writing day in school")        
+    break;
+    case "friday":
+    alert("Today is half day in school");
+    break;
+    case "saturday":
+    alert("today is a speaking class in school");
+    break;   
+    default:{
+        alert("something is wrong")
+    }      
+}
+
+// var month = prompt("\"Enter Month name please\"");
+// switch (month){
+//     case "january":
+//     alert("\"It's a first month of year\"");
+//     break;
+//     case "febuary":
+//     alert("\"It's a second month of the year");
+//     break;
+//     case "march":
+//     alert("\"It's a third month of the year\"");
+//     break;
+//     case "april":
+//     alert("\"It's a fourth month of the year\"");
+//     break;
+//     case "may":
+//     alert("\"It's a fifth month of the year\"");
+//     break;
+//     case "june":
+//     alert("\"It's a sixth month of the year\"");
+//     break;
+//     case "july":
+//     alert("\"It's a seventh month of the year\"");
+//     break;
+//     case "agust":
+//     alert("\"It's a independent  month of the year\"");
+//     break;  
+//     case "september":
+//     alert("\"It's a Ninth  month of the year\"");
+//     break;
+//     case  "octuber":
+//     alert("\"It's a tenth month of the year\"");   
+//     break;
+//     case "november":
+//     alert("\"It's a Eleventh month of the year\"");  
+//     break;
+//     case "december":
+//     alert("\"It's a Twelveth month of the year\"");       
+//     break;  
+// }
