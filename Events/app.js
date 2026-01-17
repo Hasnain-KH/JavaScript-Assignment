a1158cf5a16302bb8653514b7d3c63cd381038d5
Muhammadhasnain <webdevhasnain@gmail.com>
2026-01-17 21:57:02 +0500

    update

---
+++ Events/app.js
@@ -0,0 +1,20 @@
+function getInputvalue(){
+    var input = document.getElementById("username");
+    var inputvalue = input.value;
+    if(input.value == ""){
+        alert("please! fill the input")
+    }
+    else{
+        console.log(input.value); 
+    }
+}
+
+function getinputvalue(){
+    var get = document.getElementById("user");
+   
+    if(get.value == ""){
+        alert("Please! First Fill this Input")
+    }else{
+        console.log(get.value); 
+     }
+}     
