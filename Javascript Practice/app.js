a7f7c3243eb5981754ef063caf4bcb3269b49db9
Muhammadhasnain <webdevhasnain@gmail.com>
2026-01-27 22:37:43 +0500

    update

---
+++ Javascript Practice/app.js
@@ -0,0 +1,209 @@
+// Practice of Javascript //
+
+// let a = "hasnain";
+// let b = "hasnain"
+// console.log(a);
+// console.log(b);
+
+//  functional scope   // var is a functional Scope and let is a Block Scope// 
+
+// var b =  10;
+// function check(){
+//     var b = 20;
+// }
+// console.log(b);
+
+var d = 20;
+{
+    var d = 100;
+}
+console.log(d);
+
+
+// Reassignment  //
+
+// var a = 5;
+// console.log(a);
+// a = 10;
+// console.log(a);
+
+// let b = 15;
+// console.log(b);
+// b =  20;
+// console.log(b);
+
+// const c = 30;
+// console.log(c);
+// c = 40; 
+// console.log(c);
+
+//  hoisting //
+
+// console.log(a);
+
+// var a = 12;
+
+// const //
+
+// const a = "hassan"
+// console.log(a);
+
+// if (true){
+//     let a = 10;
+// }
+// console.log(a);
+
+// function ab(){
+//     let  h = 20;
+// }
+// console.log(h);
+
+// var ha  = 10; 
+// console.log(ha);       
+
+// var ha = 12;
+// {
+//     let a = 13;
+//     console.log(a);
+// }
+// console.log(ha);
+
+//  Const  //
+
+// const student = { Name: "Hasnain Riaz" , Age: 18};
+// console.log(student);
+
+// var Name = student.Name = "Ayan";
+// console.log(Name);
+
+// var age = student.Age = 20;
+// console.log(age);
+
+// primitive data type // jis ko copy karne par waisa hi aaye pehle jaisa
+// var  a = 12;
+// var  b = a;
+
+// var a = a + 5;
+
+// console.log(a);
+
+// var a = 15;
+// var  b = a;
+// var b = b + 5;
+
+// var a = 10;
+// console.log(a);
+// var b = a;
+// var b = b - 5;
+// console.log(b);
+
+
+//   Reference data type //
+
+// var a = [1,2,3,4,5,6];
+// var b = a;
+// b.push(7);
+
+// var a = [1,2,3,4,5,6];
+// var b = a;
+// b.pop()
+
+// var a = "Ayan";
+// var b = a;
+
+// var b = "riaz"
+
+//data types //  Primitive and Reference data type
+
+//string Number Bolean null undefine symbol bigint
+
+// Reference data types//
+
+//Arrays function (){}[]
+
+//  bigint //
+
+// var a = 223449895859598n;
+// console.log(a);
+// var a = a + 10n;
+// console.log(a);
+
+
+// let a = 292839482983982n;
+// console.log(a);
+// let b = a + 18n;
+// console.log(b);
+
+
+// var hasnain = null;
+// console.log(hasnain);
+// var hasnain = "Ayan";
+// console.log(hasnain);
+
+// let has = {
+//     vid: 1,
+//     name: "hasnain"
+    
+// }
+
+// has.vid = 1
+
+// let a1 = Symbol("hasnain");
+// let a2 = Symbol("hasnain");
+
+// var b1 = Symbol("hasnain");
+// var b2 = Symbol("hasnain");
+
+// let obj = {
+//     Name: "hasnain",
+//     age: 17,
+// }
+// let age = Symbol("age");
+// obj[age] = 20;
+
+// let has = {
+//    Name : "hasnain",
+//     father : "Riaz",
+//     age : 17,
+// }
+// has.age = 20;
+
+// Refrence data type //
+
+// let a = {
+//     Name: "hasnain",
+// }
+// let b = a;
+// b.Name = "Ayan";
+
+//  Bigint  Practice  //
+
+// var a = 9007199254740991n; 
+// var b = a + 4n;
+// console.log(a);
+// console.log(b);
+
+// let a = 9007199254740991n;
+// console.log(a);
+// let b = a + 7n;
+// console.log(b);
+
+// var a = 9007199254740991n;
+// var b = a + 9n;
+// console.log(a);
+// console.log(b)
+//;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
