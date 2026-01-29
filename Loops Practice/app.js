f97897fbfafb2abc15acebf1758eb53f1746ff7c
Muhammadhasnain <webdevhasnain@gmail.com>
2026-01-29 23:17:40 +0500

    update

---
+++ Loops Practice/app.js
@@ -0,0 +1,109 @@
+// Javascript Loops Practice // 
+
+// For even Numbers //
+// for(var i = 0; i < 21; i++){
+//    if(i % 2 === 0){
+//     console.log(i);
+//    }
+// }
+
+// For Odd Numbers //
+
+// for(var i = 0; i < 21; i++){
+//     if(i % 2 === 1){
+//         console.log(i);
+//     }
+// }
+
+// Table of 2 //
+
+// for(var i = 1; i < 11; i++){
+//     console.log(`2 * ${i} = ${2*i}`);
+// }
+
+// for(var i = 1; i < 11; i++){
+//     console.log(`5 * ${i} = ${5 * i}`);
+// }
+
+// break and Continue //
+ 1
+// for(var i = 1; i < 21; i++){
+//     console.log(i);
+//     if(i % 7 === 0){
+//         break;
+//     }    
+//     console.log(i);
+// }
+
+// for (var i = 1; i < 11; i++){
+//     console.log(i);
+//     if(i === 5){
+//         break
+//     }
+// }
+
+// for (var i = 0; i < 21; i++){
+//     if(i % 3 === 0) continue;
+//         console.log(i)
+// }
+
+// for(var i = 0; i < 11; i++){
+//     if(i % 4 === 0) continue; 
+//     console.log(i);
+// }
+
+// for (var i = 0; i < 11; i++){
+//     if(i % 5 === 0) continue ;
+//     console.log(i);
+// }
+
+// First seven Odd Numbers //
+
+// var count = 0;
+// for(var i = 0; i < 31; i++){
+//     if (i % 2 === 1){
+//         count++
+//         console.log(i);
+//     }
+//     if(i === 8)
+//         break;
+// }
+
+// for(var i = 0; i < 11; i++){
+//     if(i === 8) continue;
+//     console.log(i);   
+// }
+
+// var user = prompt("Enter here a number");
+// for(var i = 1; i <= user; i++){
+//     if(user === ""){
+//       console.log(hfsljf); 
+//     } else if(i % 2 === 0){
+//         console.log(`Even ${i}`);   
+//     }
+//     else{
+//         console.log(`Odd ${i}`);      
+//     }
+// }
+    
+// for(var i = 0; i < 31; i++){
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log(i);
+//     }   
+// }
+
+// var sum = 0;
+// for(var i = 0 ; i < 30; i++){
+//     sum = sum + 1;
+// }
+// console.log(i);
+
+
+
+
+
+
+
+
+
+
