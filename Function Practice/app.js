48f4ea2609d61bddc21e519f4475b03c0e178523
Muhammadhasnain <webdevhasnain@gmail.com>
2026-01-30 15:17:04 +0500

    update

---
+++ Function Practice/app.js
@@ -0,0 +1,119 @@
+// Practice Of Javascript //
+// Loops //
+
+// let count = 0;
+// for(var i = 0; i < 31; i++){
+//     if(i % 2 === 1){
+//         count++;
+//         console.log(i);    
+//     }
+//     if(count === 7){
+//         break;
+//     }
+// }
+
+// let val = 0;
+// for(var i = 0; i < 21; i++){
+//     if(i % 2 === 0){
+//         val++;
+//         console.log(i); 
+//     }
+//     if(val === 4){
+//         break;
+//     }
+// }
+
+// Functions Practice //
+
+// function abc(){
+//     console.log("hi"); 
+// }
+// abc();
+// abc();
+
+// function sub(v2){
+//     console.log(`${v2} padraha hun`);
+// }
+// sub("me");
+// sub("Wo");
+// sub("ALi");
+// sub("Mera Bhai");
+// sub("Ye Larka");
+
+// function practice(first){
+//     console.log(`${first} Javacript ki practice karta hun`); 
+// }
+// practice("Me");
+
+// Add Two Numbers //
+
+// function add(first, second){
+//     console.log(first + second);
+// }
+// add(2,5);
+// add(3,4);
+
+// Subtract Two Numbers //
+
+// function Sub(val1 , val2){
+//     console.log(val1 - val2);
+    
+// }
+// Sub(20,10);
+// Sub(30,20);
+
+// Multiplification of Two Numbers //
+
+// function Mul(first, second){
+//     console.log(first * second);
+    
+// }
+// Mul(3,4);
+// Mul(4,3);
+
+// Divide Two Numbers //
+
+// function divide(num1, num2){
+//     console.log(num1 / num2);
+// }
+// divide(10,2);
+// divide(20,4)
+
+// Modul Two Numbers //
+
+// function mod(start, last){
+//     console.log(start % last);
+    
+// }
+// mod(3, 3);
+// mod(4, 9);
+// mod(2, 10);
+
+// function abc(v1 , v2){
+//     console.log(v1 , v2);   
+// }
+// abc(); // It will come Undefine //
+
+// function value(v1,v2 ,v3){
+//     console.log(v1,v2,v3);
+// }
+// value(2,4); // The answer will be 2 2 undefine // 
+
+// function para(first , second){ 
+//     console.log(first + second);
+// }
+// para(); // The Answer will be NaN //
+
+// function def(one = 0 , two = 0){ // Default //
+//     console.log(one + two);
+// }
+// def();
+
+// Rest //
+
+// function rest(...value){ // Rest
+//     console.log(...value);
+// }
+// rest(1,2,3,4,5,6,7);
+// rest(10,20,30,40,50,60,70);
+// rest(100,200,300,400,500,600,700);
