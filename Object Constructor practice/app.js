fac4b02835a05a0b8984fcd591ca041803651c58
Muhammadhasnain <webdevhasnain@gmail.com>
2026-02-03 00:09:38 +0500

    update

---
+++ Object Constructor practice/app.js
@@ -0,0 +1,66 @@
+// // object Constructor Practice //
+
+// function Student(name, father, age, roll){
+//     this.name = name;
+//     this.father = father;
+//     this.age = age;
+//     this.roll = roll
+// }
+
+// let my = new Student('hasnain', 'riaz', 21, 12345);
+// let name = new Student('Sufyan', 'riaz', 17, 6789);
+// let his = new Student('Ayan', 'Riaz', 10,12345);
+// let stu = new Student('Kashif', 'Riaz', 25, 457433);
+// let they = new Student('Tahir', 'riaz', 30, 2096899);
+
+// console.log(my);
+// console.log(name); 
+// console.log(his);
+// console.log(stu);
+// console.log(they);
+
+
+// function vehical(name, model, color){
+//     this.name = name;
+//     this.model = model;
+//     this.color = color;
+// }
+// let name1 = new vehical('mehran', 2025, 'white');
+// let name2 = new vehical('cultus', 2024, 'Grey');
+// let name3 = new vehical('Alto', 2026, 'yellow');
+// let name4 = new vehical('Gli',  2026, 'Black');
+
+// console.log(name1);
+// console.log(name2);
+// console.log(name3);
+// console.log(name4);
+
+function mobile(name, price){
+    this.name = name;
+    this.price = price;
+}
+let mobile1 = new mobile('Infinix', 20000);
+let mobile2 = new mobile('Vivo', 3000000);
+let mobile3 = new mobile('Redmi', 21000);
+let mobile4 = new mobile('Nokia', 10000);
+let mobile5 = new mobile('Sumsing', 40000)
+
+console.log(mobile1);
+console.log(mobile2);
+console.log(mobile3);
+console.log(mobile4);
+console.log(mobile5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
