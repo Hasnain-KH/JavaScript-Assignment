71f5eaf31d00ee76e2d39839f9953a6e36077a2a
Muhammadhasnain <webdevhasnain@gmail.com>
2026-02-02 23:04:41 +0500

    update

---
+++ Objects Practice/app.js
@@ -0,0 +1,54 @@
+// Practice of Object //
+
+// simple syntex //
+
+// var student  ={
+//     Name: "Hasnain",
+//     FatherName: "Riaz",
+//     RollNo: 486246,
+//     Phone: +947362773,
+//     enroll: [1,2,3,4]
+// }
+
+// console.log(student.FatherName);
+
+var mobile = {
+    brand: "iphone",
+    price: 80000,
+    color: "white",
+    infinix: {
+        brand: "infinix",
+        color: "grey",
+        price: 50000,
+        sumsing: {
+            brand: "sumsin",
+            color: "Black",
+            price: 70000 
+        }
+    }
+};
+
+console.log(mobile);
+console.log(mobile.infinix);
+console.log(mobile.infinix.sumsing);
+
+let car = {
+    name: "mehran",
+    model: 2026,
+    price: 1500000,
+    condition: "Well"
+}
+
+console.log(car.condition);
+
+
+
+
+
+
+
+
+
+
+
+
