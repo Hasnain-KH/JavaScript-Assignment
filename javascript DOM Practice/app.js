16192c2dfd69063332490f2d3867211ecfae0736
Muhammadhasnain <webdevhasnain@gmail.com>
2026-02-03 22:52:18 +0500

    update

---
+++ javascript DOM Practice/app.js
@@ -0,0 +1,88 @@
+// Dom Practice //
+
+//Elements practice // 
+
+// let h1 = document.createElement("h1");
+// h1.textContent = "hi my name is hasnain riaz";
+// document.body.append(h1);
+
+// let h2 = document.createElement("h2");
+// h2.innerHTML = "Hi How are you";
+// console.log(h2.innerText);
+
+// let h3 = document.createElement("h3");
+// h3.textContent = "My Name is Hasnain";
+// document.body.append(h3);
+
+// let h4 = document.createElement("h4");
+// h4.textContent = "His name is sufyan";
+// document.body.append(h4);
+
+// let h5 = document.createElement("h5");
+// h5.textContent = "His your name is Ayan";
+// document.body.append(h5);
+
+// let h6 = document.createElement("h6");
+// h6.textContent = "My Name is Hasnain Riaz";
+// document.body.append(h6);
+
+// // append // -->  last me aata hain 
+
+// let p = document.createElement("p");
+// p.textContent = "This is a laptop";
+// document.body.append(p);
+
+// prepand // --> Start me aata hain;
+
+// let p = document.createElement("p");
+// p.innerHTML = "This is your laptop";
+// console.log(p.innerHTML);
+
+// By Id We can call // 
+
+// let name = document.getElementById("name");
+// console.log(name.textContent);
+
+// By Class Name //
+
+// let name1 = document.getElementsByClassName("name1");
+// console.log(name1);
+
+// by tag Name //
+
+// let tag = document.getElementsByTagName("p");
+// tag.innerHTML = "His Name is Sufyan";
+// console.log(tag);
+
+//By  querySelector //
+
+// let selector = document.querySelector("p");
+// selector.textContent = "Hi What's your Name";
+// console.log(selector.innerHTML);
+
+// By querySelectorAll //
+
+// let querySelectorAll = document.querySelectorAll("p");
+// querySelectorAll.texrContent = "Hi";
+// console.log(querySelectorAll);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
