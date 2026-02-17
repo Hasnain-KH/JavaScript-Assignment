cc42b95b3f780f885f3d88336ccc628b1d3c06df
Muhammadhasnain <webdevhasnain@gmail.com>
2026-02-17 12:54:02 +0500

    update

---
+++ Character Counter/app.js
@@ -0,0 +1,7 @@
+var num = 0;
+function showtext(){
+    var text = document.getElementById("myinput").value;
+    num++;
+    document.getElementById("output").innerText = text;
+    document.getElementById("counter").innerText = counter; 
+}
