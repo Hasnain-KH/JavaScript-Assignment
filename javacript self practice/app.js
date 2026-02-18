f79ae7a5afca2f98363f7fe57b4847c99e6c0d0d
Muhammadhasnain <webdevhasnain@gmail.com>
2026-02-18 16:04:03 +0500

    update

---
+++ javacript self practice/app.js
@@ -0,0 +1,63 @@
+// Practice of javascript
+
+// var myname = document.getElementById("first");
+// myname = "hasnain Riaz";
+// document.body.append(myname);
+
+
+// var element = document.createElement("div");
+// var tag = document.createElement("p");
+// var tag2 = document.createElement("p");
+
+// element.append(tag);
+// element.append(tag2);
+
+// document.body.appendChild(element);
+
+
+// var mydiv = document.createElement("div");
+// var h1 = document.createElement("h1")
+// var h1text = "My name is Hasnain";
+
+// h1.style.backgroundColor = "red"
+// h1.style.fontSize= "50px"
+
+// h1.append(h1text);
+// mydiv.appendChild(h1);
+
+// document.body.appendChild(mydiv);
+
+
+var div2 = document.createElement("div");
+var para = document.createElement("p");
+para.innerText = "Hi How are you."
+div2.append(para);
+
+para.style.color = "white";
+para.style.fontWeight = "bolder";
+para.style.display = "flex";
+para.style.justifyContent = "ce;nter";
+para.style.alignItems = "center"
+
+div2.style.backgroundColor = "red";
+div2.style.width = "150px";
+div2.style.height = "100px";
+
+document.body.appendChild(div2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
