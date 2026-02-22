63e8489a18921352f0f2228b4b92fdc1aa5193dd
Muhammadhasnain <webdevhasnain@gmail.com>
2026-02-23 00:23:32 +0500

    update

---
+++ revision practice/app.js
@@ -0,0 +1,38 @@
+// var cars = {
+//     name: "mehran",
+//     price: "100_00_0",
+//     color: ["grey, white"],
+//     model: "2024"
+// }
+// cars.isavailible = "yes";
+
+// console.log(cars);
+// console.log(cars.price);
+// console.log(cars["model"]);
+
+// var Student = function car(name){
+//     this.name = name;
+//     this.naem = name;
+//     this.name = name;
+//     this.greet = function(){
+//         console.log(`Hello My name is ${name}`);
+        
+//     }
+// }
+// var student1 = new Student('hasnain');
+// var student2 = new Student('Ayan');
+// var student3 = new Student('sufyan')
+// student1.greet();
+// student2.greet();
+// student3.greet();
+
+
+var Names = function(name){
+    this.name = name;
+}
+
+var numm = new Names("riaz khan");
+console.log(numm);
+
+
+
