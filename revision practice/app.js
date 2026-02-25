349da62183cfef1d67177d4c62db71ba31dbdc12
Muhammadhasnain <webdevhasnain@gmail.com>
2026-02-25 07:17:41 +0500

    update

--- revision practice/app.js
+++ revision practice/app.js
@@ -1,30 +1,30 @@
-// var cars = {
-//     name: "mehran",
-//     price: "100_00_0",
-//     color: ["grey, white"],
-//     model: "2024"
-// }
-// cars.isavailible = "yes";
+var cars = {
+    name: "mehran",
+    price: "100_00_0",
+    color: ["grey, white"],
+    model: "2024"
+}
+cars.isavailible = "yes";
 
-// console.log(cars);
-// console.log(cars.price);
-// console.log(cars["model"]);
+console.log(cars);
+console.log(cars.price);
+console.log(cars["model"]);
 
-// var Student = function car(name){
-//     this.name = name;
-//     this.naem = name;
-//     this.name = name;
-//     this.greet = function(){
-//         console.log(`Hello My name is ${name}`);
+var Student = function car(name){
+    this.name = name;
+    this.naem = name;
+    this.name = name;
+    this.greet = function(){
+        console.log(`Hello My name is ${name}`);
         
-//     }
-// }
-// var student1 = new Student('hasnain');
-// var student2 = new Student('Ayan');
-// var student3 = new Student('sufyan')
-// student1.greet();
-// student2.greet();
-// student3.greet();
+    }
+}
+var student1 = new Student('hasnain');
+var student2 = new Student('Ayan');
+var student3 = new Student('sufyan')
+student1.greet();
+student2.greet();
+student3.greet();
 
 
 var Names = function(name){
@@ -34,5 +34,38 @@ var Names = function(name){
 var numm = new Names("riaz khan");
 console.log(numm);
 
+function gotopage(){
+    location.href = "https://www.google.com";
+}
+
+function gotoanother(){
+    location.href = "https://www.facebook.com";
+}
+
+function gotowiki(){
+    location.href = "https://www.wikipedia.com";
+}
+
+function gotowhatsapp(){
+    location.href = "https://www.whatsapp.com";
+}
+function gotodaraz(){
+    location.href = "https://www.daraz.com";
+}
+function gotojunaidjamshed(){
+    location.href = "https://www.junaidjamshed.com";
+}
+
+function logout(){
+    location.replace("https://www.amazon.com")
+}
+
+function finish(){
+    location.replace("https://www.Netflix.com");
+}
+
+function logout2(){
+    location.replace("https://www.wikipedia.come")
+}
 
 
