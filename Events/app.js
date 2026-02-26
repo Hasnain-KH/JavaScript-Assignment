ecccec5609eff67b09ca69446a42977c77777f1e
Muhammadhasnain <webdevhasnain@gmail.com>
2026-02-26 23:31:44 +0500

    update

--- Events/app.js
+++ Events/app.js
@@ -1,20 +1,39 @@
-function getInputvalue(){
-    var input = document.getElementById("username");
-    var inputvalue = input.value;
-    if(input.value == ""){
-        alert("please! fill the input")
-    }
-    else{
-        console.log(input.value); 
-    }
-}
+// function getInputvalue(){
+//     var input = document.getElementById("username");
+//     var inputvalue = input.value;
+//     if(input.value == ""){
+//         alert("please! fill the input")
+//     }
+//     else{
+//         console.log(input.value); 
+//     }
+// }
 
-function getinputvalue(){
-    var get = document.getElementById("user");
+// function getinputvalue(){
+//     var get = document.getElementById("user");
    
-    if(get.value == ""){
-        alert("Please! First Fill this Input")
-    }else{
-        console.log(get.value); 
-     }
-}     
+//     if(get.value == ""){
+//         alert("Please! First Fill this Input")
+//     }else{
+//         console.log(get.value); 
+//      }
+// }     
+
+// function get(){
+//     var text = document.getElementById("text")
+//     console.log(text.value);
+    
+// }
+
+// function get(){
+//     var text = document.getElementById("me");
+//     console.log(text.value);
+    
+// }
+
+function hasnain(){
+    var text = document.getElementById("one").value
+    
+    console.log(text.trim());
+    
+}t
