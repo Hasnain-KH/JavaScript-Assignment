5cf8cd1d6d8c065a123cb122e6998f4ac48c596f
Muhammadhasnain <webdevhasnain@gmail.com>
2026-02-28 22:34:57 +0500

    update

--- revision practice/app.js
+++ revision practice/app.js
@@ -68,4 +68,77 @@ function logout2(){
     location.replace("https://www.wikipedia.come")
 }
 
+var h1 = document.querySelector("#name");
+h1.classList.add("h1");
+
+// var a = document.querySelector("a");
+a.classList.add("ancer");
+
+
+let img = document.createElement("img");
+document.body.appendChild(img);
+img.setAttribute("src", "assets/google.jpg");
+img.classList.add("googleimg");
+
+let img2 = document.createElement("img");
+document.body.appendChild(img2);
+img2.setAttribute("src", "assets/linkedin.png");
+img2.classList.add("linkedin");
+
+
+let a = document.createElement("a");
+let img3 = document.createElement("img");
+
+document.body.append(a);
+document.body.append(img3);
+
+a.append(img3);
+
+img3.setAttribute("src","assets/linkedin.png");
+
+a.setAttribute("href", "https://www.linkedin.com");
+
+img3.classList.add("img3");
+
+let ancer = document.createElement("a");
+let img4 = document.createElement("img");
+
+document.body.append(ancer);
+document.body.append(img4);
+
+ancer.append(img4);
+
+ancer.setAttribute("href", "https://www.github.com");
+
+
+img4.setAttribute("src", "assets/github icon.png");
+
+img4.classList.add("forimg4");
+
+
+let a5 = document.createElement("a");
+let img5 = document.createElement("img");
+
+document.body.append(a5);
+document.body.append(img5);
+
+a5.append(img5);
+
+img5.setAttribute("src", "assets/google.jpg");
+
+a5.setAttribute("href", "https://www.google.com");
+
+img5.classList.add("lastimg")
+
+
+
+
+
+
+
+
+
+
+
+
 
