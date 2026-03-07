5982054db567b4c3055ba1939f4ed8966de36586
Muhammadhasnain <webdevhasnain@gmail.com>
2026-03-07 23:45:58 +0500

    add some more tasks

--- input eventlistner practice/app.js
+++ input eventlistner practice/app.js
@@ -24,7 +24,34 @@ input.addEventListener("input", function(){
 
 let input1 = document.querySelector("#input1");
 
-input1.addEventListener("input", function(data){
-    console.log(data);
-    
+input1.addEventListener("input", function(detals){
+        console.log(detals.data);     
 })
+
+// Task 02
+
+let input2 = document.querySelector("#input2");
+
+input2.addEventListener("focus", function(){
+    alert("nothig you can write here till the time.")
+})
+
+// Task 03
+
+let input3 = document.querySelector("#input3")
+
+input3.addEventListener("input", function(){
+    console.log(input3.value.length);      
+})
+
+// Task 03
+
+let input4 =document.querySelector("#input4");
+
+input4.addEventListener("input", function(){
+    if(input4.value === "a"){
+        console.log("you Typed A letter");
+    }
+})
+
+
