e1e650780180a5877c3aba20e69f61161f9be93a
Muhammadhasnain <webdevhasnain@gmail.com>
2026-03-10 06:49:12 +0500

    Add block the element and none

--- project/app.js
+++ project/app.js
@@ -1,10 +1,19 @@
 let dice = ["⚀", "⚁","⚂","⚃","⚄","⚅"];
 let btn = document.querySelector("#btn");
+let display = document.querySelector("#display");
 
 btn.addEventListener("click", function(){
+    display.innerHTML = "";
     let dicelength = Math.floor(Math.random()*dice.length);  
-    console.log(dice[dicelength]);
-    
+    display.append(dice[dicelength]);
 })
+
+let span = document.querySelector("#span");
+let forspan = document.querySelector(".forspan");
+
+btn.addEventListener("click", function(){
+   forspan.style.display = 'block';
+})
+
 
 
