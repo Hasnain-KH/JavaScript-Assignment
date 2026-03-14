8f6c5204733b5820b971ea84a1478cf1b8b702f2
Muhammadhasnain <webdevhasnain@gmail.com>
2026-03-14 05:29:48 +0500

    update

--- Eventlistner Change/app.js
+++ Eventlistner Change/app.js
@@ -1,12 +1,16 @@
 let h1 = document.querySelector("h1");
 
 window.addEventListener("keydown", function(details){
-    if(details.key === " "){
-        h1.textContent = "YOU CLICK THE SPACE";
+    details.preventDefault();
+        if(details.key === " "){
+        h1.textContent = "I CLICK THE SPACE";
+        h1.style.letterSpacing = "-3px"
     }
     else{
         h1.textContent = details.key
-        h1.style.letterSpacing = "-2px";
+        h1.style.letterSpacing = "-3px";
         h1.style.fontSize = "130px";
+        h1.style.userSelect = "none"
+        h1.style.touppercase()
     }
 })
