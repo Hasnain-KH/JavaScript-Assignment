751f152632b6f7c60df15715233ba0b8dc48cb59
Muhammadhasnain <webdevhasnain@gmail.com>
2026-03-19 08:30:28 +0500

    Add Some more Practice of setInterval

--- SetInterval/app.js
+++ SetInterval/app.js
@@ -28,4 +28,69 @@ let timeint = setInterval(function(){
 
 clearInterval(timeint);
 
+let span = document.querySelector("span");
+let count = 11;
 
+let timer = setInterval(function(){
+   if(count >= 1){
+      count--; 
+
+      let heading5 = document.createElement("h5");
+      heading5.textContent = count;
+      span.append(heading5);
+   }else{
+      clearInterval(timer)
+   } 
+}, 1000)
+
+let h2 = document.querySelector("h2");
+
+let nam = "hasnain";
+let cl = setInterval(function(){
+   h2.append(nam) 
+}, 10000)
+
+clearInterval(cl);
+
+// practice of .repeat() Method
+
+console.log("Add\n".repeat(6));
+
+let nm = "Hasnain\n";
+alert(nm.repeat(3));
+
+let user = "Ayan\n";
+console.log(user.repeat(3));
+
+
+let user1 = "Karachi";
+let inp = document.querySelector("input");
+let btn = document.querySelector("button");
+let display = document.querySelector("#display");
+
+
+btn.addEventListener("click", function(){
+   if(inp.value.trim() === "Hasnain"){
+      display.append(user1.repeat(5));
+   }
+   else{
+      alert("Somethig Wrong")
+   }  
+})
+
+
+let load = document.getElementById("loading");
+
+let count2 = 5;
+
+let clear = setInterval(function(){
+   if(count2 > 1){
+      count2--;
+   load.append(count2);
+   }
+   else{
+      clearInterval(clear);
+   }
+}, 1000)
+
+   
