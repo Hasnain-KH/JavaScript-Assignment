c0b6ed7baa34c97405a6ea1cb3c454279d54c865
Muhammadhasnain <webdevhasnain@gmail.com>
2026-03-20 14:18:08 +0500

    Add Some more practice of setTimeout

--- SetInterval/app.js
+++ SetInterval/app.js
@@ -1,96 +1,105 @@
-let timeout = setTimeout(function(){
-    console.log("Hi hasnain"); 
-}, 4000)
+// let timeout = setTimeout(function(){
+//     console.log("Hi hasnain"); 
+// }, 4000)
 
-clearTimeout(timeout);
+// clearTimeout(timeout);
 
-let tm = setTimeout(function(){
-    alert("Hi My name is hasnain");
-}, 2000)
+// let tm = setTimeout(function(){
+//     alert("Hi My name is hasnain");
+// }, 2000)
 
-clearTimeout(tm);
+// clearTimeout(tm);
 
-let tmdel = setTimeout(function(){
-    console.log("Hi");  
-}, 1000)
+// let tmdel = setTimeout(function(){
+//     console.log("Hi");  
+// }, 1000)
 
-clearTimeout(tmdel);
+// clearTimeout(tmdel);
 
-let int = setInterval(function(){
-    console.log("HI");
-}, 2000)
+// let int = setInterval(function(){
+//     console.log("HI");
+// }, 2000)
 
-clearInterval(int);
+// clearInterval(int);
 
-let timeint = setInterval(function(){
-    console.log("Oh");
-}, 3000)
+// let timeint = setInterval(function(){
+//     console.log("Oh");
+// }, 3000)
 
-clearInterval(timeint);
+// clearInterval(timeint);
 
-let span = document.querySelector("span");
-let count = 11;
+// let span = document.querySelector("span");
+// let count = 11;
 
-let timer = setInterval(function(){
-   if(count >= 1){
-      count--; 
+// let timer = setInterval(function(){
+//    if(count >= 1){
+//       count--; 
 
-      let heading5 = document.createElement("h5");
-      heading5.textContent = count;
-      span.append(heading5);
-   }else{
-      clearInterval(timer)
-   } 
-}, 1000)
+//       let heading5 = document.createElement("h5");
+//       heading5.textContent = count;
+//       span.append(heading5);
+//    }else{
+//       clearInterval(timer)
+//    } 
+// }, 1000)
 
-let h2 = document.querySelector("h2");
+// let h2 = document.querySelector("h2");
 
-let nam = "hasnain";
-let cl = setInterval(function(){
-   h2.append(nam) 
-}, 10000)
+// let nam = "hasnain";
+// let cl = setInterval(function(){
+//    h2.append(nam) 
+// }, 10000)
 
-clearInterval(cl);
+// clearInterval(cl);
 
-// practice of .repeat() Method
+// // practice of .repeat() Method
 
-console.log("Add\n".repeat(6));
+// console.log("Add\n".repeat(6));
 
-let nm = "Hasnain\n";
-alert(nm.repeat(3));
+// let nm = "Hasnain\n";
+// alert(nm.repeat(3));
 
-let user = "Ayan\n";
-console.log(user.repeat(3));
-
-
-let user1 = "Karachi";
-let inp = document.querySelector("input");
-let btn = document.querySelector("button");
-let display = document.querySelector("#display");
+// let user = "Ayan\n";
+// console.log(user.repeat(3));
 
 
-btn.addEventListener("click", function(){
-   if(inp.value.trim() === "Hasnain"){
-      display.append(user1.repeat(5));
-   }
-   else{
-      alert("Somethig Wrong")
-   }  
-})
+// let user1 = "Karachi";
+// let inp = document.querySelector("input");
+// let btn = document.querySelector("button");
+// let display = document.querySelector("#display");
 
 
-let load = document.getElementById("loading");
+// btn.addEventListener("click", function(){
+//    if(inp.value.trim() === "Hasnain"){
+//       display.append(user1.repeat(5));
+//    }
+//    else{
+//       alert("Somethig Wrong")
+//    }  
+// })
 
-let count2 = 5;
 
-let clear = setInterval(function(){
-   if(count2 > 1){
-      count2--;
-   load.append(count2);
-   }
-   else{
-      clearInterval(clear);
-   }
-}, 1000)
+// let load = document.getElementById("loading");
 
+// let count2 = 5;
+
+// let clear = setInterval(function(){
+//    if(count2 > 1){
+//       count2--;
+//    load.append(count2);
+//    }
+//    else{
+//       clearInterval(clear);
+//    }
+// }, 1000)
+
+//  console.log("Ayan1");
+//  console.log("Ayan2");
+//  setTimeout(() =>{
+//    let a = 5;
+//    let b = 5;
+//    console.log(a + b);
+//  }, 0)
+//  console.log("Ayan3");
+ 
    
