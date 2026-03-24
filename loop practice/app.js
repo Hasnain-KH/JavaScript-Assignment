2b1e22402d4de276a6f02637415c64a3110b1bf4
Muhammadhasnain <webdevhasnain@gmail.com>
2026-03-25 02:30:12 +0500

    Add four tasks of for loop

---
+++ loop practice/app.js
@@ -0,0 +1,41 @@
+// Loop pratice Task 01:
+
+// for(var i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+// Task 02: 
+
+// for(var i = 10; i > 0; i--){
+//     console.log(i);
+// }
+
+// Task 03:
+// first way to print even numbers 1 to 20. 
+
+// for(var i = 1; i <= 21; i++){
+// if(i % 2 === 0){
+//     console.log(`even number ${i}`);
+// }
+// }
+
+// Second way to print even numbers 1 to 20. 
+
+// for(var i = 2; i <= 20; i += 2){
+//     console.log(i); 
+// }
+
+// Task 04:
+// first way to print odd numbers 1 to 20. 
+
+// for(var i = 0; i <= 20; i++){
+//     if(i % 3 === 0){
+//         console.log(i);    
+//     }
+// }
+
+// Second way to print odd numbers 1 to 20. 
+
+for(var i = 0; i <= 20; i += 3){
+    console.log(i);
+}
