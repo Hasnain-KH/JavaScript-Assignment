72f83f3969a7e40a58248d99a20784f8658c80e0
Muhammadhasnain <webdevhasnain@gmail.com>
2026-04-02 16:37:38 +0500

    I try to make the navbar by bootstrap

---
+++ resturant app/faaltoo/project no 2/app.js
@@ -0,0 +1,62 @@
+//  var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+//  var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+//  var fullNames = [];
+//  for (var i = 0; i < firstNames.length; i++) {
+//  for (var j = 0; j < lastNames.length; j++) {
+//  fullNames.push(firstNames[i] + lastNames[j]);
+//  }
+// } 
+
+// var firstNames = ["ayan", "sufyan", "hasnain"]
+// var lastNames = ["khan", "khan", "khan", "khan"];
+// for(var i = 0; i < firstNames.length; i++){
+//     for(var j = 0; i > lastNames.length; i++){
+//         console.log(fullNames(firstNames[i] + lastNames[j]));
+        
+//     }
+// }
+
+
+//  var i = 0;
+//  while (i <= 3) {
+//  alert(i);
+//  i++;
+//  }
+
+//  var ha = 0;
+//  while(i <= 5){
+//     alert(i);
+//     i++
+//  }
+
+//  var has = 0;
+//  while(has < 6){
+//    console.log(has);
+//    has++
+//  }
+
+//  var has = 3;
+// while(has < 6){
+//   alert("hi it's nice");
+//   has++
+// } 
+
+// function doSum(num1 , num2)
+//  {
+//  return(num1 + num2);
+//  }
+//  var ans = doSum(24,5);
+//  document.write(ans);
+
+//  function has (a , b){
+//   return(a + b)
+//  }
+// var nas = has(20 , 30);
+// alert(nas)
+
+
+
+
+
+
+
